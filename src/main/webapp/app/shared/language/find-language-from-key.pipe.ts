6a939787417471b6081c871417105f34ffcc7275
John Doe <johndoe@example.com>
2020-03-22 17:14:42 +0100

    Initial version of BugTrackerJHipster generated by JHipster-6.8.0

---
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'findLanguageFromKey' })
+export class FindLanguageFromKeyPipe implements PipeTransform {
+  private languages: { [key: string]: { name: string; rtl?: boolean } } = {
+    'ar-ly': { name: 'العربية', rtl: true },
+    'zh-cn': { name: '中文（简体）' },
+    'zh-tw': { name: '繁體中文' },
+    en: { name: 'English' }
+    // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
+  };
+
+  transform(lang: string): string {
+    return this.languages[lang].name;
+  }
+
+  isRTL(lang: string): boolean {
+    return Boolean(this.languages[lang].rtl);
+  }
+}
